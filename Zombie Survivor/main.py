048b56a9f6b893677a725a2f5a76bcba129d1894
srapisardi <50967041+srapisardi@users.noreply.github.com>
2021-07-22 19:15:47 -0400

    Added empty clip sound and zombie animations

--- Zombie Survivor/main.py
+++ Zombie Survivor/main.py
@@ -7,6 +7,7 @@ class Player(games.Sprite):
     image = games.load_image("player.png")
     sound_shoot = games.load_sound("sounds/pistol.wav")
     sound_reload = games.load_sound("sounds/reload.wav")
+    sound_empty = games.load_sound("sounds/empty_gun.wav")
     NEXT_SHOT = 25
 
     def __init__(self):
@@ -35,11 +36,13 @@ class Player(games.Sprite):
 
 
     def update(self):
+        #Moving Left and Right
         if games.keyboard.is_pressed(games.K_a):
             self.x -= 5
         if games.keyboard.is_pressed(games.K_d):
             self.x += 5
 
+        #Shooting
         if self.shoot_wait > 0:
             self.shoot_wait -= 1
         if games.keyboard.is_pressed(games.K_SPACE) and self.shoot_wait == 0 and self.ammo_count.value > 0:
@@ -49,11 +52,18 @@ class Player(games.Sprite):
             self.shoot_wait = Player.NEXT_SHOT
             self.ammo_count.value -= 1
 
-        if games.keyboard.is_pressed(games.K_r) and self.ammo_count.value == 0:
+        #Empty Clip
+        if games.keyboard.is_pressed(games.K_SPACE) and self.shoot_wait == 0 and self.ammo_count.value == 0:
+            Player.sound_empty.play()
+            self.shoot_wait = Player.NEXT_SHOT
+
+        #Reload
+        if games.keyboard.is_pressed(games.K_r) and self.ammo_count.value == 0 and self.shoot_wait == 0:
             self.ammo_count.value = 20
             Player.sound_reload.play()
+            self.shoot_wait = Player.NEXT_SHOT
 
-
+        #Stops Player from going off screen
         if self.left < 0:
             self.left = 0
         if self.right > games.screen.width:
@@ -69,15 +79,65 @@ class Bullet(games.Sprite):
                                      y = y,
                                      dy = -10)
 
+    def update(self):
+        for sprite in self.overlapping_sprites:
+            sprite.destroy()
+            self.destroy()
+            death = ZombieDeath(sprite.x, sprite.y)
+            games.screen.add(death)
+
+
+
+class ZombieWalk(games.Animation):
+    images = ["zombie/walk/walk0001.png", "zombie/walk/walk0002.png", "zombie/walk/walk0003.png", "zombie/walk/walk0004.png", "zombie/walk/walk0005.png",
+              "zombie/walk/walk0006.png", "zombie/walk/walk0007.png", "zombie/walk/walk0008.png", "zombie/walk/walk0009.png", "zombie/walk/walk0010.png",
+              "zombie/walk/walk0011.png", "zombie/walk/walk0012.png", "zombie/walk/walk0013.png", "zombie/walk/walk0014.png", "zombie/walk/walk0015.png",
+              "zombie/walk/walk0016.png", "zombie/walk/walk0017.png", "zombie/walk/walk0018.png", "zombie/walk/walk0019.png", "zombie/walk/walk0020.png",
+              "zombie/walk/walk0021.png", "zombie/walk/walk0022.png", "zombie/walk/walk0023.png", "zombie/walk/walk0024.png", "zombie/walk/walk0025.png",
+              "zombie/walk/walk0026.png", "zombie/walk/walk0027.png", "zombie/walk/walk0028.png", "zombie/walk/walk0029.png", "zombie/walk/walk0030.png", "zombie/walk/walk0031.png"]
+
+
+    def __init__(self, x, y):
+        super(ZombieWalk, self).__init__(images = ZombieWalk.images,
+                                         x = x,
+                                         y = y,
+                                         angle = 90,
+                                         dy = 3,
+                                         n_repeats = 0,
+                                         repeat_interval = 1)
+
+
+
+
+class ZombieDeath(games.Animation):
+    images = ["zombie/death01/death01_0001.png", "zombie/death01/death01_0002.png", "zombie/death01/death01_0003.png", "zombie/death01/death01_0004.png",
+              "zombie/death01/death01_0005.png", "zombie/death01/death01_0006.png", "zombie/death01/death01_0007.png", "zombie/death01/death01_0008.png",
+              "zombie/death01/death01_0009.png", "zombie/death01/death01_0010.png", "zombie/death01/death01_0011.png", "zombie/death01/death01_0012.png",
+              "zombie/death01/death01_0013.png", "zombie/death01/death01_0014.png", "zombie/death01/death01_0015.png", "zombie/death01/death01_0016.png"]
+
+    def __init__(self, x, y):
+        super(ZombieWalk, self).__init__(images = ZombieDeath.images,
+                                         x = x,
+                                         y = y,
+                                         angle = 90,
+                                         n_repeats = 1,
+                                         repeat_interval = 1)
 
 
 def main():
     bg = games.load_image("background.jpg")
     games.screen.background = bg
 
+    games.music.load("sounds/music.wav")
+    games.music.play(-1)
+
 
     p = Player()
     games.screen.add(p)
+
+    z = ZombieWalk(400,0)
+    games.screen.add(z)
+
     games.screen.mainloop()
 
 
